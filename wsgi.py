bdd71abbc9d18c9faa843a576e713d46b74c7972
Fi <fi.ahmed@icloud.com>
2023-07-09 21:16:19 +0100

    listening at port 10000

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,4 @@ from api import app
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 10000))
